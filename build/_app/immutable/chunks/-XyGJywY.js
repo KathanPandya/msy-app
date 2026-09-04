8039ea5b2048a8a28c54a3c7474e525e56822598
Ankit Bhatt <info@arktechnolabs.com>
2026-09-04 20:12:08 +0530

    - added new github action to deploy to lightsail directly. - also removed build folder from git - updated gitignore file

--- build/_app/immutable/chunks/-XyGJywY.js
+++
@@ -1,1 +0,0 @@
-import{y as o,z as n}from"./CIr2Bsvm.js";const e=Symbol("member-shell");function r(t){n(e,t)}function l(){return o(e)}export{l as g,r as s};
--- build/_app/immutable/entry/start.DL1a4ky8.js
+++
@@ -1,1 +0,0 @@
-import{s as r}from"../chunks/DswE2sAh.js";import{x as t}from"../chunks/NkpZys6s.js";export{t as load_css,r as start};
